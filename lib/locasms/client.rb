9d8e7c64e591ecaa8b4c3b2b1b4685453d73bd4a
Adilson Carvalho <adilson@adilsoncarvalho.com.br>
2013-10-12 16:41:37 -0300

    Accepting initialization parameters

--- lib/locasms/client.rb
+++ lib/locasms/client.rb
@@ -1,6 +1,31 @@
 module LocaSMS
 
+  # Client to interact with LocaSMS API
   class Client
+    # Default API address
+    ENDPOINT = 'http://173.44.33.18/painel/api.ashx'
+
+    attr_reader :login, :password
+
+    # @param [String] login authorized user
+    # @param [String] password access password
+    # @param [Hash] opts
+    # @option opts :rest_client (RestClient) client to be used to handle http requests
+    def initialize(login, password, opts={})
+      @login    = login
+      @password = password
+
+      @rest = opts[:rest_client]
+    end
+
+  private
+
+    # Gets the current RestClient to handle http requests
+    # @return [RestClient] you can set on class creation passing it on the options
+    # @private
+    def rest
+      @rest ||= RestClient.new ENDPOINT, lgn: login, pwd: password
+    end
   end
 
 end
--- spec/lib/locasms/client_spec.rb
+++ spec/lib/locasms/client_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe LocaSMS::Client do
-  it 'Should have tests :)'
+
+  describe '.initialize' do
+    subject { LocaSMS::Client.new :login, :password }
+    it { subject.login.should be(:login) }
+    it { subject.password.should be(:password) }
+  end
+
 end
