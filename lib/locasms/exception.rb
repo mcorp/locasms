75b81d4616a230e3aee43ec7fd7af502324d3d9b
Adilson Carvalho <adilson@adilsoncarvalho.com.br>
2013-10-15 00:29:51 -0300

    Created some basic exceptions

--- lib/locasms/exception.rb
+++ lib/locasms/exception.rb
@@ -1,6 +1,13 @@
 module LocaSMS
 
-  class Exception < ::Exception
-  end
+  # Common base exception
+  class Exception < ::Exception; end
+
+  # Raised when asked for an invalid operation
+  # @see https://github.com/mcorp/locasms/wiki/A-API-de-envio#lista-das-a%C3%A7%C3%B5es-dispon%C3%ADveis
+  class InvalidOperation < Exception; end
+
+  # Raised when the given credentials are invalid
+  class InvalidLogin < Exception; end
 
 end
