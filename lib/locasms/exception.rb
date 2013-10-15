9d7cfbb55d4e869d2c2b4f9cc19a405dddff136f
Adilson Carvalho <adilson@adilsoncarvalho.com.br>
2013-10-15 01:32:47 -0300

    Added other exceptions to keep validation simple

--- lib/locasms/exception.rb
+++ lib/locasms/exception.rb
@@ -1,13 +1,36 @@
 module LocaSMS
 
   # Common base exception
-  class Exception < ::Exception; end
+  class Exception < ::Exception
+    attr_reader :raw, :action
+
+    def initialize(data = {})
+      @raw    = data[:raw]
+      @action = data[:data]
+
+      super data[:message] || default_message
+    end
+
+  private
+
+    def default_message
+      nil
+    end
+  end
 
   # Raised when asked for an invalid operation
   # @see https://github.com/mcorp/locasms/wiki/A-API-de-envio#lista-das-a%C3%A7%C3%B5es-dispon%C3%ADveis
-  class InvalidOperation < Exception; end
+  class InvalidOperation < Exception
+    def default_message
+      'Invalid Operation'
+    end
+  end
 
   # Raised when the given credentials are invalid
-  class InvalidLogin < Exception; end
+  class InvalidLogin < Exception
+    def default_message
+      'Invalid Login'
+    end
+  end
 
 end
