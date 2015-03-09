df23c957258b8c829e33643f8a73d5f6923e4283
Leonardo Saraiva <vyper@maneh.org>
2015-03-09 13:26:05 +0000

    Added exceptions to autoload

--- lib/locasms.rb
+++ lib/locasms.rb
@@ -6,10 +6,12 @@ autoload :RestClient, 'rest_client'
 autoload :Logger,     'logger'
 
 module LocaSMS
-  autoload :Client,     'locasms/client'
-  autoload :Exception,  'locasms/exception'
-  autoload :Numbers,    'locasms/numbers'
-  autoload :RestClient, 'locasms/rest_client'
+  autoload :Client,            'locasms/client'
+  autoload :Exception,         'locasms/exception'
+  autoload :InvalidLogin,      'locasms/exception'
+  autoload :InvalidOperation,  'locasms/exception'
+  autoload :Numbers,           'locasms/numbers'
+  autoload :RestClient,        'locasms/rest_client'
 
   module Helpers
     autoload :DateTimeHelper, 'locasms/helpers/date_time_helper'
